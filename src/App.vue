8822eedd6ac0879613a4795e4273ebe9c6f7ec21
OlaJFrick <info@culturehero.org>
2017-05-03 10:25:20 +0200

    Uploaded almost empty project - Martin will rename the project to "Test Vue Routing"

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <!-- <router-view></router-view> -->
+    <h2>{{title}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+      return {
+        title: 'Experimenting with Routing'
+      }
+    },
+    components: {
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import 'sass/main.scss';
+</style>
